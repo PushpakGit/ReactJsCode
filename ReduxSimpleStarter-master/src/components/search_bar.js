d31dc808735c58ae221153749ff1a004f10593a2
Pushpak.Waghmare <pushpak.waghmare@harbingergroup.com>
2018-01-11 12:32:25 +0530

    Commit Change of Class and constructor

--- ReduxSimpleStarter-master/src/components/search_bar.js
+++ ReduxSimpleStarter-master/src/components/search_bar.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const SearchBar = () => {
-    return <textArea />
+class SearchBar extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = { term: ''}; 
+        
+    }
+
+    render() {
+        return <input onChange={event =>  console.log(event.target.value)} />;
+    }
 }
 
 export default SearchBar;
